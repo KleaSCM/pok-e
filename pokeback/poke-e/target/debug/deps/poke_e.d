cc967b81e5a932020413740112622c3763b3d5d9
Your Name <john.farmer1086@gmail.com>
2024-09-05 12:16:19 +0800

    init

---
+++ pokeback/poke-e/target/debug/deps/poke_e.d
@@ -0,0 +1,5 @@
+C:\Users\Kliea\Documents\Development\Poke\pokeback\poke-e\target\debug\deps\poke_e.exe: src/main.rs
+
+C:\Users\Kliea\Documents\Development\Poke\pokeback\poke-e\target\debug\deps\poke_e.d: src/main.rs
+
+src/main.rs:
---
+++ pokeback/poke-e/target/debug/poke-e.d
@@ -0,0 +1,1 @@
+C:\Users\Kliea\Documents\Development\Poke\pokeback\poke-e\target\debug\poke-e.exe: C:\Users\Kliea\Documents\Development\Poke\pokeback\poke-e\src\main.rs
